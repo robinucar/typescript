2508a06e05027e143f0037cb91b0051bf2683344
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-06-23 13:07:43 +0100

    any

---
+++ any.js
@@ -0,0 +1,5 @@
+// type can be anything,
+// not recommended to use, should be avoid if it is possible
+var activities;
+activities = ['sports', true, 10];
+console.log(activities);
---
+++ any.ts
@@ -0,0 +1,6 @@
+// type can be anything,
+// not recommended to use, should be avoid if it is possible
+
+let activities: any[];
+activities = ['sports', true, 10]
+console.log(activities)
